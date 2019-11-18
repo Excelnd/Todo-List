0cf7b3b6ffc0c2e07864dc36264957bc5d6cdd86
Excelnd <ihsanmpm@gmail.com>
2019-11-18 22:43:33 +0530

    added Edit, View option to the table.

--- src/main/java/ihs/learnprogramming/controller/TodoItemController.java
+++ src/main/java/ihs/learnprogramming/controller/TodoItemController.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -44,8 +41,16 @@ public class TodoItemController {
     }
 
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model) {
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id,
+                              Model model) {
+
+        log.info("editing id = {}", id);
+        TodoItem todoItem = todoItemService.getItem(id);
+
+        if(todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
+
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
@@ -53,7 +58,26 @@ public class TodoItemController {
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("todoItem from from = {}", todoItem);
-        todoItemService.addItem(todoItem);
+
+        if(todoItem.getId() == 0) {
+            todoItemService.addItem(todoItem);
+        } else {
+            todoItemService.updateItem(todoItem);
+        }
         return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        log.info("Deleteing item with it= {}", id);
+        todoItemService.removeItem(id);
+        return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.VIEW_ITEM)
+    public String viewItem(@RequestParam int id, Model model) {
+        TodoItem todoItem = todoItemService.getItem(id);
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.VIEW_ITEM;
     }
 }
--- src/main/webapp/WEB-INF/view/items_list.jsp
+++ src/main/webapp/WEB-INF/view/items_list.jsp
@@ -1,10 +1,14 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="ihs.learnprogramming.util.Mappings" %>
 <html>
 <head>
     <title>Todo Items</title>
 </head>
 <body>
     <div align="center">
+    <c:url var="addUrl" value="${Mappings.ADD_ITEM}"/>
+    <a href="${addUrl}">New Item</a>
+
         <table border="1" cellpadding="5">
 
             <caption><h2>Todo Items</h2></caption>
@@ -12,13 +16,31 @@
             <tr>
                 <th>Title</th>
                 <th>DeadLine</th>
+                <th>View</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
 
             <c:forEach var="item" items="${todoData.items}">
 
+                <c:url var="viewUrl" value="${Mappings.VIEW_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+
+                <c:url var="editUrl" value="${Mappings.ADD_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+
+                <c:url var="deleteUrl" value="${Mappings.DELETE_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+
                 <tr>
                     <td><c:out value="${item.title}"/></td>
                     <td><c:out value="${item.deadline}"/></td>
+                    <td><a href="${viewUrl}"/>View</td>
+                    <td><a href="${editUrl}"/>Edit</td>
+                    <td><a href="${deleteUrl}">Delete</a></td>
                 </tr>
 
             </c:forEach>
